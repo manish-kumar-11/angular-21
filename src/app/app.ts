67cc8c8c9449b05e0fb19c622f7f5a89603f9a58
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2026-05-07 13:49:48 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule,ButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular-v-20');
+  data:string = 'Angular v20';
 }

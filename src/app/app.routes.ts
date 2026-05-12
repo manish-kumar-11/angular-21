2bb843261e201e19c827aacea0e69c6c177453f7
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2026-05-12 15:59:13 +0530

    router learning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+path:'',
+component:App
+   },
+    {
+        path: 'about',
+        loadComponent: () => import('./components/about/about').then(m => m.About)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./components/contact/contact').then(m => m.Contact)
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
@@ -19,7 +19,7 @@ interface Person{
 }
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardModule, ButtonModule, CommonModule, Child],
+  imports: [RouterLink, RouterOutlet, CardModule, ButtonModule, CommonModule, Child],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
